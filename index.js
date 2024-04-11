50aa1580c0bfda70a8ca7cff1423abcc28c11b8f
lkphuong <90914246+lkphuong@users.noreply.github.com>
2024-04-11 19:33:14 +0700

    Update dotenv dependency to version 16.4.5 in package-lock.json

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
+console.log(process.env.TOKEN);
+
 const bot = new TelegramBot(process.env.TOKEN, {
   polling: true,
 });
